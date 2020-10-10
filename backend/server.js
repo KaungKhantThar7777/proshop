b13117a45e70076a6956af8fdb0f67a5db5c7bf2
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-10 13:32:00 +0630

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -31,8 +27,19 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (_req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 const __dirname = path.resolve();
-console.log(__dirname);
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
